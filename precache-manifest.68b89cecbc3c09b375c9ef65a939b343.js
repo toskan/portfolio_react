a193360a51e44c6e17565c8884bf2551aa94cae4
toskan <tamar.oskanian@gmail.com>
2020-04-16 09:00:21 -0400

    Updates

---
+++ precache-manifest.68b89cecbc3c09b375c9ef65a939b343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde73c7f36d06a7b62313ea72dc143f9",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "0882737ecbd0967d67a4",
+    "url": "/portfolio_react/static/css/main.9c6c4ef1.chunk.css"
+  },
+  {
+    "revision": "075c729f0d912ca40af5",
+    "url": "/portfolio_react/static/js/2.7e29b37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.7e29b37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0882737ecbd0967d67a4",
+    "url": "/portfolio_react/static/js/main.8a7e6e0e.chunk.js"
+  },
+  {
+    "revision": "8ad38dfdeabc498f2de6",
+    "url": "/portfolio_react/static/js/runtime-main.3e6fd58f.js"
+  }
+]);
