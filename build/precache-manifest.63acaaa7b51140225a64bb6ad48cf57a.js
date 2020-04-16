c8911e49b7f2bcf018545c8992a4fdf480aee056
toskan <tamar.oskanian@gmail.com>
2020-04-16 14:24:47 -0400

    added files

---
+++ build/precache-manifest.63acaaa7b51140225a64bb6ad48cf57a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e263d2c747de3e92234b2dc3f63bdfb",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "0171e57b8f1b0e2e32ee",
+    "url": "/portfolio_react/static/css/main.9c6c4ef1.chunk.css"
+  },
+  {
+    "revision": "075c729f0d912ca40af5",
+    "url": "/portfolio_react/static/js/2.7e29b37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.7e29b37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0171e57b8f1b0e2e32ee",
+    "url": "/portfolio_react/static/js/main.a92e76da.chunk.js"
+  },
+  {
+    "revision": "8ad38dfdeabc498f2de6",
+    "url": "/portfolio_react/static/js/runtime-main.3e6fd58f.js"
+  }
+]);
---
+++ src/images.js
@@ -0,0 +1,30 @@
+const images = [
+	[
+		'portfolio_react/img/egypt_screenshot.mobile.PNG',
+		'portfolio_react/img/egypt_2_screenshot.PNG',
+		'portfolio_react/img/egypt_screenshot.PNG',
+	],
+	[
+		'portfolio_react/img/greco_screenshot_mobile_2.PNG',
+		'portfolio_react/img/greco_screenshot_mobile.PNG',
+		'portfolio_react/img/greco_screenshot.PNG',
+	],
+	[
+		'portfolio_react/img/map_api_cell_screenshot_2.PNG',
+		'portfolio_react/img/map_api_cell_screenshot_3.PNG',
+		'portfolio_react/img/mobile_map_api_screenshot.PNG',
+		'portfolio_react/img/map_api_screenshot.PNG',
+		'portfolio_react/img/map_api_cell_screenshot.PNG',
+	],
+	[
+		'portfolio_react/img/news_api_screenshot_mobile_2.PNG',
+		'portfolio_react/img/news_api_screenshot_mobile.PNG',
+		'portfolio_react/img/news_api_screenshot.PNG',
+	],
+	[
+		'portfolio_react/img/weather_app_screenshot_2.PNG',
+		'portfolio_react/img/weather_app_screenshot.PNG',
+	],
+];
+
+export default images;
