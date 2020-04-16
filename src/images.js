9c4f3a701d3ec4f6f4e32e0d67704437c48f03eb
toskan <tamar.oskanian@gmail.com>
2020-04-16 10:30:13 -0400

    img files fixed for local

--- src/images.js
+++ src/images.js
@@ -1,27 +1,30 @@
 const images = [
 	[
-		'/public/img/egypt_screenshot.mobile.PNG',
-		'../images/egypt_2_screenshot.PNG',
-		'../images/egypt_screenshot.PNG',
+		'portfolio_react/img/egypt_screenshot.mobile.PNG',
+		'portfolio_react/img/egypt_2_screenshot.PNG',
+		'portfolio_react/img/egypt_screenshot.PNG',
 	],
 	[
-		'./public/img/greco_screenshot_mobile_2.PNG',
-		'../images/greco_screenshot_mobile.PNG',
+		'portfolio_react/img/greco_screenshot_mobile_2.PNG',
+		'portfolio_react/img/greco_screenshot_mobile.PNG',
 		'../images/greco_screenshot.PNG',
 	],
 	[
-		'../public/img/map_api_cell_screenshot_2.PNG',
-		'../images/map_api_cell_screenshot_3.PNG',
-		'../images/mobile_map_api_screenshot.PNG',
-		'../images/map_api_screenshot.PNG',
-		'../images/map_api_cell_screenshot.PNG',
+		'portfolio_react/img/map_api_cell_screenshot_2.PNG',
+		'portfolio_react/img/map_api_cell_screenshot_3.PNG',
+		'portfolio_react/img/mobile_map_api_screenshot.PNG',
+		'portfolio_react/img/map_api_screenshot.PNG',
+		'portfolio_react/img/map_api_cell_screenshot.PNG',
 	],
 	[
-		'./img/news_api_screenshot_mobile_2.PNG',
-		'../images/news_api_screenshot_mobile.PNG',
-		'../images/news_api_screenshot.PNG',
+		'portfolio_react/img/news_api_screenshot_mobile_2.PNG',
+		'portfolio_react/img/news_api_screenshot_mobile.PNG',
+		'portfolio_react/img/news_api_screenshot.PNG',
 	],
-	['weather_app_screenshot_2.PNG', '../images/weather_app_screenshot.PNG'],
+	[
+		'portfolio_react/img/weather_app_screenshot_2.PNG',
+		'portfolio_react/img/weather_app_screenshot.PNG',
+	],
 ];
 
 export default images;
