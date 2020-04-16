9cebe14073220d5bac94083e88206c8594d44180
toskan <tamar.oskanian@gmail.com>
2020-04-16 09:43:00 -0400

    img bug

--- src/images.js
+++ src/images.js
@@ -1,30 +1,27 @@
 const images = [
 	[
-		'../images/egypt_screenshot.mobile.PNG',
+		'/public/img/egypt_screenshot.mobile.PNG',
 		'../images/egypt_2_screenshot.PNG',
 		'../images/egypt_screenshot.PNG',
 	],
 	[
-		'../images/greco_screenshot_mobile_2.PNG',
+		'./public/img/greco_screenshot_mobile_2.PNG',
 		'../images/greco_screenshot_mobile.PNG',
 		'../images/greco_screenshot.PNG',
 	],
 	[
-		'../images/map_api_cell_screenshot_2.PNG',
+		'../public/img/map_api_cell_screenshot_2.PNG',
 		'../images/map_api_cell_screenshot_3.PNG',
 		'../images/mobile_map_api_screenshot.PNG',
 		'../images/map_api_screenshot.PNG',
 		'../images/map_api_cell_screenshot.PNG',
 	],
 	[
-		'../images/news_api_screenshot_mobile_2.PNG',
+		'./img/news_api_screenshot_mobile_2.PNG',
 		'../images/news_api_screenshot_mobile.PNG',
 		'../images/news_api_screenshot.PNG',
 	],
-	[
-		'../images/weather_app_screenshot_2.PNG',
-		'../images/weather_app_screenshot.PNG',
-	],
+	['weather_app_screenshot_2.PNG', '../images/weather_app_screenshot.PNG'],
 ];
 
 export default images;
